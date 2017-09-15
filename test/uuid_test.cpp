2fee559401fddbd9fdc2d9023ee889c860e79a75
Carlos Martin Sanchez <carlosvin@gmail.com>
2017-09-15 20:19:20 +0200

    Modified project to work with doctest instead of catch

--- test/uuid_test.cpp
+++ test/uuid_test.cpp
@@ -1,42 +1,38 @@
-/*
- * uuid.cpp
- *      Author: carlos
- */
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-
+#include "doctest.h"
 #include "Uuid.h"
-#include "catch.hpp"
 #include <string>
 
 constexpr int MAX_ITERS = 100;
 
-TEST_CASE( "Uuid", "[uuid]" ) {
+TEST_CASE( "Uuid" ) {
   for (int i=0; i<MAX_ITERS; i++) {
     ids::Uuid uuid;
     std::string uuid_str {uuid.to_str()};
 
-    INFO(uuid_str);
-    REQUIRE(uuid.most > 0);
-    REQUIRE(uuid.least > 0);
-    REQUIRE(uuid_str.size() == 36);
+    MESSAGE(uuid_str);
+    CHECK(uuid.most > 0);
+    CHECK(uuid.least > 0);
+    CHECK(uuid_str.size() == 36);
   }
 }
+// SECTION in catch = SUBCASE in doctest
 
 // BDD style
 
-SCENARIO( "UUID creation", "[Uuid]" ) {
-  
-  ids::Uuid uuid;
-  std::string uuid_str {uuid.to_str()};
+SCENARIO( "UUID creation" ) {
 
-  GIVEN( "A random UUID " + uuid_str) {
-    REQUIRE(uuid_str.size() == 36);
+  GIVEN( "A random UUID " ) {
+    ids::Uuid uuid;
+    std::string uuid_str {uuid.to_str()};
+
+    CHECK(uuid_str.size() == 36);
 
 	  WHEN( "get the most and least" ) {
 		  THEN( "should be more than 0" ) {
-			  REQUIRE( uuid.most > 0);
-			  REQUIRE( uuid.least > 0);
+			  CHECK( uuid.most > 0);
+			  CHECK( uuid.least > 0);
 		  }
 	  }
   }  
