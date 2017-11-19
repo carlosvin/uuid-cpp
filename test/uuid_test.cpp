84bae377f09ff841a32a06bb5fd87a3b1be65454
Carlos Martin Sanchez <carlsa@microsoft.com>
2017-11-19 12:07:17 +0100

    update file format

--- test/uuid_test.cpp
+++ test/uuid_test.cpp
@@ -1,23 +1,24 @@
-/*
- * uuid.cpp
- *      Author: carlos
- */
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-
-#include "Uuid.h"
 #include "catch.hpp"
+#include "Uuid.h"
 #include <string>
 
 constexpr int MAX_ITERS = 100;
 
-TEST_CASE( "Uuid", "[uuid]" ) {
-  for (int i=0; i<MAX_ITERS; i++) {
+TEST_CASE("Uuid", "[uuid]")
+{
+  for (int i = 0; i < MAX_ITERS; i++)
+  {
     ids::Uuid uuid;
-    std::string uuid_str {uuid.to_str()};
+    std::string uuid_str{uuid.to_str()};
 
     INFO(uuid_str);
+
+    // If assertion fails test execution is stopped
     REQUIRE(uuid_str.size() == 36);
+
+    // If assertion fails test execution continues
     CHECK(uuid.most > 0);
     CHECK(uuid.least > 0);
   }
@@ -25,19 +26,24 @@ TEST_CASE( "Uuid", "[uuid]" ) {
 
 // BDD style
 
-SCENARIO( "UUID creation", "[Uuid]" ) {
-  
-  ids::Uuid uuid;
-  std::string uuid_str {uuid.to_str()};
+SCENARIO("UUID creation", "[Uuid]")
+{
 
-  GIVEN( "A random UUID " + uuid_str) {
+  GIVEN("A random UUID ")
+  {
+
+    ids::Uuid uuid;
+    std::string uuid_str{uuid.to_str()};
+
     REQUIRE(uuid_str.size() == 36);
 
-	  WHEN( "get the most and least" ) {
-		  THEN( "should be more than 0" ) {
-			  CHECK( uuid.most > 0);
-			  CHECK( uuid.least > 0);
-		  }
-	  }
-  }  
+    WHEN("get the most and least")
+    {
+      THEN("should be more than 0")
+      {
+        CHECK(uuid.most > 0);
+        CHECK(uuid.least > 0);
+      }
+    }
+  }
 }
